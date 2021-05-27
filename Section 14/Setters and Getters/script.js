71a786ccf94eeaf4d777fa845f532b2af1d8c722
jehanshub <83107988+jehanshub@users.noreply.github.com>
2021-05-27 23:06:28 +0530

    Add files via upload

---
+++ Section 14/Setters and Getters/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// Setters and Getters
+const account = {
+  owner: 'Jonas',
+  movements: [200, 530, 120, 300],
+
+  //get
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  //set
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
